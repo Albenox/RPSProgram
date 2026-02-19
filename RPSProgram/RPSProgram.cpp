cfb898974fdeda5813a751f2979f129d06139760
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-02-19 15:32:31 -0500

    Has the functionality of a working game, without as much notes, interface cleanliness, and a proper ending function to be complete.

--- RPSProgram/RPSProgram.cpp
+++ RPSProgram/RPSProgram.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <random>
+#include <cstdlib>
 
 using namespace std;
 
 void promptDisplay(int userScore, int computerScore, int roundCount);
+int userInput(int userChoice);
+int computerInput(int computerChoice);
 int logic(int userChoice, int computerChoice);
+void postgame(int computerChoice, int userScore, int computerScore, int winner);
 
 int main()
 {
 	// Main variables for the game
 	int userScore = 0;
 	int computerScore = 0;
-	int userChoice;
-	int computerChoice;
+	int userChoice = 0;
+	int computerChoice = 0;
+	int winner;
 	int roundCount = 0;
 	bool gameOver = 0;
 	
 	// Main loop that runs until a winner is determined or until the user decides to quit
 	while (gameOver != 1) {
-		std::cout << "Hello World!\n";
+		promptDisplay(userScore, computerScore, roundCount);
+		cin >> userChoice;
+		userChoice = userInput(userChoice);
+		computerChoice = computerInput(computerChoice);
+		winner = logic(userChoice, computerChoice);
+		postgame(computerChoice, userScore, computerScore, winner);
 	}
 	
 	
@@ -27,7 +36,7 @@ int main()
 // Function to display the game prompt and current scores
 void promptDisplay(int userScore, int computerScore, int roundCount) {
 	if (userScore == 0 && computerScore == 0) {
-		cout << "Welcome to Rock, Paper, Scissors!" << endl;
+		cout << endl << "Welcome to Rock, Paper, Scissors!" << endl;
 		cout << "You will be playing against the computer to score points, and the first to 3 points wins!" << endl;
 	}
 	cout << endl << "------------------------------" << endl;
@@ -35,7 +44,35 @@ void promptDisplay(int userScore, int computerScore, int roundCount) {
 	cout << "Your Score: " << userScore << endl;
 	cout << "Computer Score: " << computerScore << endl;
 	cout << "The computer is ready to make their move, please enter your choice between: " << endl;
-	cout << "1. Rock" << endl << "2. Paper" << endl << "3. Scissors" << endl;
+	cout << "1. Rock" << endl << "2. Paper" << endl << "3. Scissors" << endl << endl;
+}
+
+// Function to allow the user to input their choice and validate it
+int userInput(int userChoice) {
+	int firstCheck = 1;
+	while(userChoice < 1 || userChoice > 3 || firstCheck == 1){
+		switch (userChoice) {
+		case 1:
+			return 1; // Rock
+			firstCheck = 0;
+		case 2:
+			return 2; // Paper
+			firstCheck = 0;
+		case 3:
+			return 3; // Scissors
+			firstCheck = 0;
+		default:
+			cout << "Invalid input, please enter a number between 1 and 3." << endl << endl;
+			cin >> userChoice;
+			firstCheck = 0;
+		}
+	}
+}
+
+// Function to randomly generate computer choice
+int computerInput(int computerChoice) {
+	computerChoice = rand() % 3 + 1; // Generates a random number between 1 and 3
+	return computerChoice;
 }
 
 // Function that holds logic for determining the winner of each round
@@ -51,5 +88,23 @@ int logic(int userChoice, int computerChoice) {
 	}
 	else {
 		return -1; // Computer wins
+	}
+}
+
+// Function to display and update scores after each round and determine if the game is over
+void postgame(int computerChoice, int userScore, int computerScore, int winner) {
+	cout << "The computer chose " << computerChoice << ", which means " << endl;
+	switch (winner) {
+	case 0:
+		cout << "It's a tie!" << endl;
+		break;
+	case 1:
+		cout << "You win this round!" << endl;
+		userScore++;
+		break;
+	case -1:
+		cout << "The computer wins this round!" << endl;
+		computerScore++;
+		break;
 	}
 }

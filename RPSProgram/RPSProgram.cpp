eca6bf215512b0ef8e4b171eb0bad779af03b72d
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-02-19 13:04:39 -0500

    Set up the basic variables, logic, and prompt display, without yet connecting anything together.

--- RPSProgram/RPSProgram.cpp
+++ RPSProgram/RPSProgram.cpp
@@ -1,20 +1,55 @@
-// RPSProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <random>
+
+using namespace std;
+
+void promptDisplay(int userScore, int computerScore, int roundCount);
+int logic(int userChoice, int computerChoice);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Main variables for the game
+	int userScore = 0;
+	int computerScore = 0;
+	int userChoice;
+	int computerChoice;
+	int roundCount = 0;
+	bool gameOver = 0;
+	
+	// Main loop that runs until a winner is determined or until the user decides to quit
+	while (gameOver != 1) {
+		std::cout << "Hello World!\n";
+	}
+	
+	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Function to display the game prompt and current scores
+void promptDisplay(int userScore, int computerScore, int roundCount) {
+	if (userScore == 0 && computerScore == 0) {
+		cout << "Welcome to Rock, Paper, Scissors!" << endl;
+		cout << "You will be playing against the computer to score points, and the first to 3 points wins!" << endl;
+	}
+	cout << endl << "------------------------------" << endl;
+	cout << "Round: " << roundCount << endl;
+	cout << "Your Score: " << userScore << endl;
+	cout << "Computer Score: " << computerScore << endl;
+	cout << "The computer is ready to make their move, please enter your choice between: " << endl;
+	cout << "1. Rock" << endl << "2. Paper" << endl << "3. Scissors" << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Function that holds logic for determining the winner of each round
+int logic(int userChoice, int computerChoice) {
+	// Logic for determining the winner of each round
+	if (userChoice == computerChoice) {
+		return 0; // Tie
+	}
+	else if ((userChoice == 1 && computerChoice == 3) || 
+			 (userChoice == 2 && computerChoice == 1) || 
+			 (userChoice == 3 && computerChoice == 2)) {
+		return 1; // User wins
+	}
+	else {
+		return -1; // Computer wins
+	}
+}
